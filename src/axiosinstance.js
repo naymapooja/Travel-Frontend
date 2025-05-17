96ed33826d3a18a190b219048cb1fe7d058b775d
naymapooja <poojadhakad250@gmail.com>
2025-05-17 12:56:18 +0530

    Base URL bug fix

--- src/axiosinstance.js
+++ src/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: `${import.meta.env.VITE_UR}/api`,
+    baseURL: `${import.meta.env.VITE_URL}/api`,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
