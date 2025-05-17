571051f41d2e9916ac1d77e2cffd2de83c80c51a
naymapooja <poojadhakad250@gmail.com>
2025-05-17 13:02:13 +0530

    bug fix

--- src/axiosinstance.js
+++ src/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: `${import.meta.env.VITE_URL}/api`,
+    baseURL: `${import.meta.env.VITE_SERVER_BASE_URL}/api`,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
